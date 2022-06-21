ee924a9dd4c04107f8f75f3ad5cd9672f20b6b7f
Kabir seth <kabirseth81@gmail.com>
2022-06-21 21:39:48 +0530

    reinitialized files

---
+++ app/src/main/java/com/example/loginandsignup/signInActivity.kt
@@ -0,0 +1,78 @@
+package com.example.loginandsignup
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.loginandsignup.databinding.ActivitySignInBinding
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+
+class signInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener {
+            val intent = Intent(this,signUpActivity::class.java)
+            startActivity(intent)
+        }
+
+binding.forgotButton.setOnClickListener {
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle("Forgot Password")
+    val view = layoutInflater.inflate(R.layout.dialog_forgot_password,null)
+    val username =view.findViewById<EditText>(R.id.et_username)
+    builder.setView(view)
+    builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _,_  ->
+        forgotPassword(username)
+    })
+    builder.setNegativeButton("close", DialogInterface.OnClickListener { _,_  ->  })
+    builder.show()
+}
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                    firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Empty Fields Are Not Allowed.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun forgotPassword(username:EditText) {
+       if(username.text.toString().isEmpty()){
+           return
+       }
+        if(!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()) {
+            return
+        }
+        firebaseAuth.sendPasswordResetEmail(username.text.toString())
+            .addOnCompleteListener{
+if(it.isSuccessful)
+{
+    Toast.makeText(this, "Email Sent", Toast.LENGTH_SHORT).show()
+}
+                else{
+    Toast.makeText(this, "Unmatched", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/loginandsignup/signUpActivity.kt
@@ -0,0 +1,47 @@
+package com.example.loginandsignup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.loginandsignup.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class signUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, signInActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass.equals(confirmPass)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, signInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "This password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are Not Allowed.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
